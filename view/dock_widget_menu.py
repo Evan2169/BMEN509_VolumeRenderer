d674d24e3b0b6e51f63e640e0d87ec68475a4373
Kasha98 <56083109+Kasha98@users.noreply.github.com>
2020-03-23 20:35:17 -0600

    Adding sliders to our menu for adjusting parameters.

--- view/dock_widget_menu.py
+++ view/dock_widget_menu.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QDockWidget, QFileDialog, QHBoxLayout, QGroupBox, QLabel, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QDockWidget, QFileDialog, QHBoxLayout, QGroupBox, QLabel, QPushButton, QSlider, QVBoxLayout, QWidget
 from PyQt5.QtCore import QDir, Qt
+import vtk
 
 class DockWidgetMenu(QDockWidget):
     def __init__(self, main_window):
@@ -14,6 +15,8 @@ class DockWidgetMenu(QDockWidget):
         widget = QWidget(self)
 
         self.__create_file_selection_view(layout, widget)
+        self.__sharpening_filter(layout, widget)
+        self.__gaussian_filter(layout, widget)
 
         widget.setLayout(layout)
         self.setWidget(widget)
@@ -30,8 +33,63 @@ class DockWidgetMenu(QDockWidget):
 
         def open_file():
             file, _ = QFileDialog.getOpenFileName(parent, directory = QDir.currentPath())
+
             if(file):
                 path_delimited = file.split("/")
-                file_name_path_removed = path_delimited[len(path_delimited) - 1] 
+                file_name_path_removed = path_delimited[len(path_delimited) - 1]
                 filename_widget.setText(file_name_path_removed)
+
         open_file_button.clicked.connect(open_file)
+
+    def __sharpening_filter(self, layout, parent):
+
+        sharpening_groupbox = QGroupBox("Sharpening Filter", parent)
+        sharpen_label = QLabel("Sharpen", sharpening_groupbox)
+        sharpen_slider = QSlider(Qt.Horizontal)
+        sharpen_slider.setMinimum(0)
+        sharpen_slider.setMaximum(2000)
+        sharpen_slider.setValue(1000)
+
+        #sharpen_slider.valueChanged.connect(self.sharpen)
+
+        sharpen_layout = QHBoxLayout()
+        sharpen_layout.addWidget(sharpen_label)
+        sharpen_layout.addWidget(sharpen_slider)
+        sharpening_groupbox.setLayout(sharpen_layout)
+        layout.addWidget(sharpening_groupbox)
+
+    def __gaussian_filter(self, layout, parent):
+
+        gaussian_groupbox = QGroupBox("Gaussian Filter", parent)
+        gaussian_radius_label = QLabel("Radius Factor", gaussian_groupbox)
+        gaussian_radius_slider = QSlider(Qt.Horizontal)
+        gaussian_radius_slider.setMinimum(0)
+        gaussian_radius_slider.setMaximum(2000)
+        gaussian_radius_slider.setValue(1000)
+
+        gaussian_std_label = QLabel("Standard Dev.", gaussian_groupbox)
+        gaussian_std_slider = QSlider(Qt.Horizontal)
+        gaussian_std_slider.setMinimum(0)
+        gaussian_std_slider.setMaximum(2000)
+        gaussian_std_slider.setValue(1000)
+
+        gaussian_layout = QHBoxLayout()
+        gaussian_layout.addWidget(gaussian_radius_label)
+        gaussian_layout.addWidget(gaussian_radius_slider)
+
+        gaussian_layout.addWidget(gaussian_std_label)
+        gaussian_layout.addWidget(gaussian_std_slider)
+
+        gaussian_groupbox.setLayout(gaussian_layout)
+        layout.addWidget(gaussian_groupbox)
+
+        '''
+        gaussian = vtk.vtkImageGaussianSmooth()
+        gaussian.SetStandardDeviation(2)
+        gaussian.SetDimensionality(3)
+        gaussian.SetRadiusFactor(1)
+        gaussian.SetInput(imageIn.GetOutput())
+        gaussian.Update()
+        '''
+        #vtkImageGaussianSmooth
+        #vtkImageMedian3D
